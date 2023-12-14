d1b4a61f70a3c8df59e89792d761afac9fd9012b
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-15 02:28:51 +0530

    dice was changed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// create dice
 function createDice(number){
     const dotPositionMatrix={
         1:[
@@ -44,15 +46,38 @@ function createDice(number){
         dot.style.setProperty("--left",dotPosition[1]+"%");
         dice.appendChild(dot);
     }
-    return dice;
-
-    
+    return dice;  
+}
+//random number generate
+function  randomDice(){
+    const random=Math.floor((Math.random()*6)+1);
+    return random;
 }
 // console.log(createDice(6));
 const diceContent=document.querySelector(".firstDice");
 const diceContent_2=document.querySelector(".secondDice");
 
-diceContent.appendChild(createDice(3));
-diceContent_2.appendChild(createDice(3));
+////diceContent.appendChild(createDice(randomDice()));
+//diceContent_2.appendChild(createDice(randomDice()));
+
+
+//button oparation
+const btn=document.querySelector(".b1");
+btn.addEventListener("click",()=>{
+    diceContent.innerHTML="";
+    const r=randomDice();
+    diceContent.appendChild(createDice(r))
+    document.getElementById("t_1").innerHTML=r;
+    
+});
+const btn_2=document.querySelector(".b2");
+btn_2.addEventListener("click",()=>{
+    diceContent_2.innerHTML="";
+    const r=randomDice();
+    diceContent_2.appendChild(createDice(r))
+    document.getElementById("t_2").innerHTML=r;
+});
+
+
 
 
